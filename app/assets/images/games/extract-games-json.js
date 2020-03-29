4a560938ebacebce54029dbbc6b6958c33c742bc
BakedPVP <gameking36@msn.com>
2020-03-29 01:03:12 -0400

    Update analytics

--- app/assets/images/games/extract-games-json.js
+++ app/assets/images/games/extract-games-json.js
@@ -5,7 +5,7 @@ games = {}
 
 for (let g of GameStore) games[g.name] = g.id;
 
-fs.writeFile('D:/Git/Projects/Twitchcord/twitchcord.github.io/app/assets/images/games',
+fs.writeFile('E:/Git/Projects/Twitchcord/twitchcord.github.io/app/assets/images/games',
               JSON.stringify(games, null, 2), () => {
                 console.log('FILE SAVED!');
               }
